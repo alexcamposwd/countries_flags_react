b9f6211f4f43a069f9192767fd95f0458a7ea53b
Alex Campos <alexfilhotes@hotmail.com>
2022-05-03 17:56:25 -0300

    modify about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -29,38 +29,6 @@ function About() {
         </S.ProjectText>
       </S.SectionProject>
       <S.Underline />
-      <S.SectionProject>
-        <S.DevInfo>
-          <S.ProjectTitle>Autor: Alex Campos</S.ProjectTitle>
-          <S.ProjectTitle>
-            GitHub:
-            <a href='https://github.com/alexcamposwd'>
-              github.com/alexcamposwd
-            </a>
-          </S.ProjectTitle>
-          <S.DevText>
-            Sou formado em Processamento de Dados, porém fiquei longe da área
-            por 20 anos e gostaria de retornar para a área através desta
-            oportunidade oferecida pela Provu.
-            <br />
-            <spam>"Desenvolvedor em contínuo aperfeiçoamento"</spam>
-          </S.DevText>
-          <S.DevStacks>
-            <h3>Stacks:</h3>
-            <li>
-              <p>html</p>
-              <p>css</p>
-              <p>javascript</p>
-              <p>react</p>
-              <p>vue</p>
-              <p>nodeJS</p>
-              <p>typescript</p>
-              <p>mongodb</p>
-              <p>postgres</p>
-            </li>
-          </S.DevStacks>
-        </S.DevInfo>
-      </S.SectionProject>
     </S.ContainerAbout>
   )
 }
